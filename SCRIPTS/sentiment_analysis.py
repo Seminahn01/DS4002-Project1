3ebe4d691a70a953e2b5dac29c2c0bbf4b669da7
Seminahn01 <157026462+Seminahn01@users.noreply.github.com>
2025-02-13 16:00:12 -0500

    Update sentiment_analysis.py

--- SCRIPTS/sentiment_analysis.py
+++ SCRIPTS/sentiment_analysis.py
@@ -1,3 +1,10 @@
 # Perform sentiment analysis
 df["sentiment_score"] = df["review_text"].apply(lambda x: sia.polarity_scores(str(x))["compound"])
 
+# Exploratory Data Analysis (EDA)
+sns.scatterplot(data=df, x="sentiment_score", y="lifetime_gross")
+plt.title("Sentiment Score vs. Lifetime Gross Revenue")
+plt.xlabel("Sentiment Score")
+plt.ylabel("Lifetime Gross Revenue")
+plt.show()
+    
